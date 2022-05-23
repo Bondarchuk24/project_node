f939d4da6f6da23f6a2e8f682291a805f5ab6e61
Bondarchuk24 <103238135+Bondarchuk24@users.noreply.github.com>
2022-05-23 08:44:09 +0300

    (start) - my firts course project

--- routes/weapon.js
+++ routes/weapon.js
@@ -1,11 +1,7 @@
 const { cache } = require('ejs');
 const {Router} = require('express');
-const Weapon = require('../model/Weapon')
-const userController = require('../controllers/user-controller');
 const weaponController = require('../controllers/weapon-controller');
 const router = Router();
-const {body} = require('express-validator');
-const authMiddleware = require('../middleware/auth-middleware');
 
 
 // pages
@@ -20,18 +16,5 @@ router.post('/create', weaponController.createWeapon);
 router.post('/delete/:id', weaponController.deleteWeapon);
 // UPDATE
 router.post('/update/:id', weaponController.updateWeapon);
-
-// authorization
-
-router.post('/registration',
-	body('email').isEmail(),
-	body('password').isLength({min: 3, max: 32}),
-	userController.registration);
-router.post('/login', userController.login);
-router.post('/logout', userController.logout);
-router.get('/activate/:link', userController.activate);
-router.get('/refresh', userController.refresh);
-router.get('/users', authMiddleware, userController.getUsers);
-
 
 module.exports = router;
